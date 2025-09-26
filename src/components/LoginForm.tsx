f6c3ad6f2f6dd2357715d4187ef2878d9a44f587
Nghia Vo <vominhnghia1878@gmail.com>
2025-09-26 18:29:25 +0700

    style:Update Login UI

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,15 +1,15 @@
-import { useState } from "react";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "./ui/select";
-import { Button } from "./ui/button";
-import GoogleIcon from "./icons/GoogleIcons";
-import FPTIcon from "./icons/FPTIcon";
-import { useGoogleLogin } from "@react-oauth/google";
+// import { useState } from "react";
+// import {
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "./ui/select";
+// import { Button } from "./ui/button";
+// import GoogleIcon from "./icons/GoogleIcons";
+// import FPTIcon from "./icons/FPTIcon";
+// import { useGoogleLogin } from "@react-oauth/google";
 // import jwt_decode from "jwt-decode";
 
 // interface GoogleJwt {
@@ -19,69 +19,35 @@ import { useGoogleLogin } from "@react-oauth/google";
 // }
 
 const LoginForm = () => {
-  const [loginTabs, setLoginTabs] = useState("Student");
+  // const [loginTabs, setLoginTabs] = useState("Student");
 
-  const handleTabs = (tabName: string) => {
-    setLoginTabs(tabName);
-  };
+  // const handleTabs = (tabName: string) => {
+  //   setLoginTabs(tabName);
+  // };
 
-  const login = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      console.log("Google login success:", tokenResponse);
+  // const login = useGoogleLogin({
+  //   onSuccess: (tokenResponse) => {
+  //     console.log("Google login success:", tokenResponse);
 
-    },
-    onError: (error) => console.error("Login Failed:", error),
-  });
+  //   },
+  //   onError: (error) => console.error("Login Failed:", error),
+  // });
 
   return (
-    <div className="flex flex-col gap-6">
-      <div className="absolute text-xs left-4 -top-11 w-64 lg:static flex items-center font-bold shadow-xl/20 lg:w-full p-0.5 bg-[#DDDBDD] rounded-md text-[#00033D] lg:rounded-lg lg:text-base">
-        <button
-          onClick={() => handleTabs("Student")}
-          className={`py-2 flex-1 cursor-pointer rounded-l-md ${
-            loginTabs == "Student" ? "bg-white" : "bg-transparent"
-          }`}
-        >
-          Staff & Students
-        </button>
+    <div className="absolute bg-white px-14 py-12 left-0 right-0 top-0 w-fit">
+        <div className="flex flex-col gap-12">
+          <img src="/images/Login/Logo.svg" alt="" />
 
-        <button
-          onClick={() => handleTabs("Parent")}
-          className={`py-2 flex-1 cursor-pointer rounded-r-md ${
-            loginTabs == "Parent" ? "bg-white" : "bg-transparent"
-          }`}
-        >
-          Parents
-        </button>
-      </div>
-      <h1 className="text-left text-base lg:text-2xl font-bold text-[#00033D]">
-        Login For {loginTabs == "Student" ? "Staffs & Students" : "Parents"}
-      </h1>
-      <div className="flex flex-col gap-4">
-        <Select>
-          <SelectTrigger className="w-full bg-white">
-            <SelectValue placeholder="Select your campus" />
-          </SelectTrigger>
-          <SelectContent className="bg-white">
-            <SelectItem value="HN">Ha Noi</SelectItem>
-            <SelectItem value="HCM">Ho Chi Minh</SelectItem>
-            <SelectItem value="DN">Da Nang</SelectItem>
-            <SelectItem value="CT">Can Tho</SelectItem>
-          </SelectContent>
-        </Select>
-        <div className="flex flex-col gap-4 text-white">
-          <Button onClick={() => login()} className="bg-[#005EB8] font-normal cursor-pointer">
-            <GoogleIcon /> Sign in with FPT Gmail
-          </Button>
-          {loginTabs == "Student" && (
-            <Button className="bg-[#005EB8] font-normal cursor-pointer">
-              <FPTIcon /> Sign in with FEID
-            </Button>
-          )}
+          <div>
+            <h2 className="font-public text-2xl font-bold">Academic Portal</h2>
+          </div>
         </div>
-      </div>
     </div>
   );
 };
+
+{/* <Button onClick={() => login()} className="bg-[#005EB8] font-normal cursor-pointer">
+            <GoogleIcon /> Sign in with FPT Gmail
+          </Button> */}
 
 export default LoginForm;
--- src/pages/AuthTemplate/LoginPage/index.tsx
+++ src/pages/AuthTemplate/LoginPage/index.tsx
@@ -2,36 +2,49 @@ import LoginForm from "@/components/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div className="bg-[#00033D] min-h-screen relative overflow-hidden">
-      <div className="flex flex-col lg:flex-row items-center justify-between h-screen z-10 relative">
-        <div className="absolute mx-auto top-6 lg:left-1/8 lg:top-1/4">
-          <img className="size-64 lg:size-96" src="/images/logoWithName.svg" alt="" />
-        </div>
+    <div className="min-h-screen">
+      <div className="relative">
+        <img src="/images/Login/Background.png" alt=""  className="bg-cover size-full h-screen bg-center"/>
 
-        <div className="absolute bottom-0 w-full h-fit px-8 py-16 rounded-t-3xl lg:right-0 lg:w-5/12 lg:h-full bg-white lg:p-16 lg:rounded-t-none lg:rounded-tl-3xl lg:rounded-bl-3xl">
-          <div className="flex flex-col justify-center h-full gap-36">
-            <LoginForm />
-
-            <div className="flex flex-col items-center text-[#00033D] text-sm gap-2">
-                <span className="hidden lg:block">Mobile App for Student and Parent is ready at</span>
-                <div className="hidden lg:flex gap-2 p-2">
-                <button className="cursor-pointer">
-                  <img src="/images/appstore.svg" alt="" />
-                </button>
-                <button className="cursor-pointer">
-                  <img src="/images/chplay.svg" alt="" />
-                </button>
-                </div>
-              <span>Powered by Greenwich University</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="absolute translate-x-44 -translate-y-12 top-1/4 lg:inset-0 lg:flex lg:items-center lg:justify-center">
-        <img src="/images/GWLogo.svg" alt="Greenwich Logo"/>
+        <LoginForm/>
       </div>
     </div>
+    // <div className="bg-[#00033D] min-h-screen relative overflow-hidden">
+    //   <div className="flex flex-col lg:flex-row items-center justify-between h-screen z-10 relative">
+    //     <div className="absolute mx-auto top-6 lg:left-1/8 lg:top-1/4">
+    //       <img
+    //         className="size-64 lg:size-96"
+    //         src="/images/logoWithName.svg"
+    //         alt=""
+    //       />
+    //     </div>
+
+    //     <div className="absolute bottom-0 w-full h-fit px-8 py-16 rounded-t-3xl lg:right-0 lg:w-5/12 lg:h-full bg-white lg:p-16 lg:rounded-t-none lg:rounded-tl-3xl lg:rounded-bl-3xl">
+    //       <div className="flex flex-col justify-center h-full gap-36">
+    //         <LoginForm />
+
+    //         <div className="flex flex-col items-center text-[#00033D] text-sm gap-2">
+    //           <span className="hidden lg:block">
+    //             Mobile App for Student and Parent is ready at
+    //           </span>
+    //           <div className="hidden lg:flex gap-2 p-2">
+    //             <button className="cursor-pointer">
+    //               <img src="/images/appstore.svg" alt="" />
+    //             </button>
+    //             <button className="cursor-pointer">
+    //               <img src="/images/chplay.svg" alt="" />
+    //             </button>
+    //           </div>
+    //           <span>Powered by Greenwich University</span>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+
+    //   <div className="absolute translate-x-44 -translate-y-12 top-1/4 lg:inset-0 lg:flex lg:items-center lg:justify-center">
+    //     <img src="/images/GWLogo.svg" alt="Greenwich Logo" />
+    //   </div>
+    // </div>
   );
 };
 
