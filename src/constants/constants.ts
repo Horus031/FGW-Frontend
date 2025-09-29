34b4b8edc0f41c32975e3deb2c407212bf408a30
Nghia Vo <vominhnghia1878@gmail.com>
2025-09-29 19:50:28 +0700

    feat: Update request page

---
+++ src/constants/constants.ts
@@ -0,0 +1,21 @@
+export const documentOptions = [
+  { value: "Interim Academic Transcript", label: "Interim Academic Transcript" },
+  { value: "Interruption of Study", label: "Interruption of Study" },
+  { value: "Recheck of your mark", label: "Recheck of your mark" },
+  { value: "Transfer to Another Programme", label: "Transfer to Another Programme" },
+  { value: "Transfer to Another Institution", label: "Transfer to Another Institution" },
+  { value: "Transfer to Another Class", label: "Transfer to Another Class" },
+  { value: "Request for Attendance Adjustment", label: "Request for Attendance Adjustment" },
+  { value: "Other Requests", label: "Other Requests" },
+];
+
+export const verificationOptions = [
+  { value: "Interruption of Study", label: "Interruption of Study" },
+  { value: "Hoãn nghĩa vụ quân sự", label: "Hoãn nghĩa vụ quân sự" },
+  { value: "Giấy giới thiệu", label: "Giấy giới thiệu" },
+  { value: "Đề nghị Xác nhận sinh viên tiếng Anh", label: "Đề nghị Xác nhận sinh viên tiếng Anh" },
+  { value: "Đề nghị xác nhận sinh viên tiếng Việt", label: "Đề nghị xác nhận sinh viên tiếng Việt" },
+  { value: "Cấp thẻ SV tạm thời", label: "Cấp thẻ SV tạm thời" },
+  { value: "Giấy xác nhận vay vốn", label: "Giấy xác nhận vay vốn" },
+  { value: "Cấp lại thẻ SV", label: "Cấp lại thẻ SV" },
+];
