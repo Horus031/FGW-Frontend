1e90cb4fb26b55f43d1db447b48fecc53ec01d30
Nghia Vo <vominhnghia1878@gmail.com>
2025-10-23 18:20:49 +0700

    feat: Update search logic for Grade Page (Teacher)

--- src/components/shared/InputWithIcon.tsx
+++ src/components/shared/InputWithIcon.tsx
@@ -1,16 +1,19 @@
+import type { Ref } from "react";
 import MagnifyClass from "../icons/MagnifyClass";
 import { Input } from "../ui/input";
 
 type InputWithIconProps = {
     placeholder? : string;
+    ref?: Ref<HTMLInputElement> | undefined;
 };
 
 const InputWithIcon = (props: InputWithIconProps) => {
-    const { placeholder } = props
+    const { placeholder, ref } = props
   return (
     <div className="relative">
       <MagnifyClass className="absolute  top-1/2 left-4 -translate-y-1/2" />
       <Input
+        ref={ref}
         className="w-xs pl-12"
         placeholder={placeholder}
       />
