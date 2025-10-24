6db073e088939735606d8345aeba8f1f800d757e
Nghia Vo <vominhnghia1878@gmail.com>
2025-10-24 17:00:59 +0700

    Re-config vite proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-export default defineConfig(({ mode }) => {
+export default defineConfig(() => {
   // Load env variables based on the current mode (development/production)
-  const env = loadEnv(mode, process.cwd(), '')
+  // const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react(), tailwindcss()],
@@ -17,7 +17,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/requests': {
-          target: env.VITE_BACKEND_URL, // ✅ use loaded env
+          target: 'https://greenwich-ap-backend.onrender.com/api', // ✅ use loaded env
           changeOrigin: true,
           secure: false,
           rewrite: (path) => path.replace(/^\/requests/, ''),
