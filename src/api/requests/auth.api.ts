798eb65cca7d6a2b0ba6afb3382ac949015b33c5
Tai874vn <135409751+Tai874vn@users.noreply.github.com>
2025-10-24 13:30:06 +0700

    fix access token naming

--- src/api/requests/auth.api.ts
+++ src/api/requests/auth.api.ts
@@ -34,12 +34,12 @@ export const getMe = async () => {
 
 export const logout = async () => {
   try {
-    localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("refresh_token");
 
     // Nếu bạn có set cookie cho token
-    document.cookie = "accessToken=; Max-Age=0; path=/;";
-    document.cookie = "refreshToken=; Max-Age=0; path=/;";
+    document.cookie = "access_token=; Max-Age=0; path=/;";
+    document.cookie = "refresh_token=; Max-Age=0; path=/;";
 
     const response = await api.post("/auth/logout");
 
