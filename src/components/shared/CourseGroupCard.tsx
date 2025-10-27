9e92c06ee410b49e307470d05d00a6961925d430
Nghia Vo <vominhnghia1878@gmail.com>
2025-10-27 15:50:07 +0700

    fix: Resolve conversation

--- src/components/shared/CourseGroupCard.tsx
+++ src/components/shared/CourseGroupCard.tsx
@@ -1,16 +1,12 @@
-
 type CourseGroupProps = {
   courseCode: string;
   courseName: string;
 };
 
 const CourseGroupCard = (props: CourseGroupProps) => {
-    const { courseCode, courseName } = props;
+  const { courseCode, courseName } = props;
   return (
-    <div
-      key={courseCode}
-      className="px-4 py-3 rounded-lg text-xs flex flex-col gap-4 border-1 border-gray-400 cursor-pointer active:scale-95"
-    >
+    <div className="px-4 py-3 rounded-lg text-xs flex flex-col gap-4 border-1 border-gray-400 cursor-pointer active:scale-95">
       <span className="font-semibold">{courseCode}</span>
       <span>{courseName}</span>
     </div>
